c6f7a730df4355c1a3a7189e3200f818a28af97d
GarryAma <amasiagarry23@gmail.com>
2023-02-17 13:10:06 +1100

    bug fixed

--- App.js
+++ App.js
@@ -3,27 +3,27 @@ const heading1 = React.createElement(
   {
     id: "title1",
     className: "classHeading1",
-    style: { color: cyan },
+    style: { color: "cyan" },
   },
   "Heading1 made from React.createElement",
 );
 
 const heading2 = React.createElement(
-  "h1",
+  "h2",
   {
     id: "title2",
     className: "classHeading2",
-    style: { color: tomato },
+    style: { color: "tomato" },
   },
   "Heading2 made from React.createElement",
 );
 
-const container = React.createElement("div", { id: container }, [
+const container = React.createElement("div", { id: "container" }, [
   heading1,
   heading2,
 ]);
 
-const root = React.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 //passing a react element inside the root
 root.render(container);
