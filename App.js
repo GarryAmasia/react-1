6d432c9d66d52b1b30049a3e782c55c64803cdf2
GarryAma <amasiagarry23@gmail.com>
2023-02-17 12:49:18 +1100

    React createElement()

---
+++ App.js
@@ -0,0 +1,9 @@
+const heading1 = React.createElement(
+  "h1",
+  {
+    id: "title",
+    className: "classHeading1",
+    style: { color: cyan },
+  },
+  "Heading1 made from React.createElement",
+);
