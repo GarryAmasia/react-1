6131e56302b0745b4cbc4161150cc7ab2a71baf2
GarryAma <amasiagarry23@gmail.com>
2023-02-17 12:52:06 +1100

    add dependencies from createElement()

--- App.js
+++ App.js
@@ -1,9 +1,24 @@
 const heading1 = React.createElement(
   "h1",
   {
-    id: "title",
+    id: "title1",
     className: "classHeading1",
     style: { color: cyan },
   },
   "Heading1 made from React.createElement",
 );
+
+const heading2 = React.createElement(
+  "h1",
+  {
+    id: "title2",
+    className: "classHeading2",
+    style: { color: tomato },
+  },
+  "Heading2 made from React.createElement",
+);
+
+const container = React.createElement("div", { id: container }, [
+  heading1,
+  heading2,
+]);
